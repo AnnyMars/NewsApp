b93b609e31929cde00cbaffe905c756521f31478
AnnyMars <korpanyuk97@bk.ru>
2023-10-27 11:39:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.newsapp.util
+
+object Constants {
+    const val USER_SETTINGS = "userSettings"
+    const val APP_ENTRY = "appEntry"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "49ca188611b74f7cbc7b0bfb1a7bf7b4"
+
+    const val NEWS_DATABASE_NAME = "news_db"
+}
